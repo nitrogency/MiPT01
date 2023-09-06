1f5657c4f5cf2143a3c76dab0ed261e38547a48c
nitrogency <nitrog@protonmail.com>
2023-09-06 18:08:45 +0300

    Added button which changes text to "Hello"

--- app/src/main/java/com/example/mipt01/MainActivity.java
+++ app/src/main/java/com/example/mipt01/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.mipt01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvHello = (TextView)findViewById(R.id.tvHello);
+    }
+
+    public void onBtnChangeTextClick(View view){
+        tvHello.setText("Hello");
     }
 }
